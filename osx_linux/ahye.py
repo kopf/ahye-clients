42357c59cf70a118db32b12573bfb373f40ceaa7
Aengus Walton <ventolin@gmail.com>
2013-10-27 21:46:15 +0100

    Add OS X (darwin) support

--- osx_linux/ahye.py
+++ osx_linux/ahye.py
@@ -2,13 +2,20 @@
 import os
 import tempfile
 import subprocess
+import sys
 
 import requests
 
 
 COMMANDS = {
-    'browser': 'x-www-browser {url}',
-    'screenshot': 'gnome-screenshot -a -f {filename}'
+    'linux2': {
+        'browser': 'x-www-browser {url}',
+        'screenshot': 'gnome-screenshot -a -f {filename}'
+    },
+    'darwin': {
+        'browser': 'open {url}',
+        'screenshot': 'screencapture -s {filename}'
+    }
 }
 
 PROXIES = {
@@ -27,11 +34,11 @@ VERSION = '1.0'
 
 if __name__ == '__main__':
     tmp_file = tempfile.mkstemp(suffix='.png')[1]
-    subprocess.call(COMMANDS['screenshot'].format(filename=tmp_file).split(' '))
+    subprocess.call(COMMANDS[sys.platform]['screenshot'].format(filename=tmp_file).split(' '))
     files = {'imagedata': open(tmp_file, 'rb')}
     headers = {'User-Agent': 'ahye-python {0}'.format(VERSION)}
     url = '{0}:{1}{2}'.format(SERVER['host'], SERVER['port'], SERVER['path'])
     resp = requests.post(url, auth=SERVER['auth'], headers=headers,
                          files=files, proxies=PROXIES, verify=False)
-    subprocess.Popen(COMMANDS['browser'].format(url=resp.text).split(' '))
+    subprocess.Popen(COMMANDS[sys.platform]['browser'].format(url=resp.text).split(' '))
     os.remove(tmp_file)
