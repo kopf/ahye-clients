27c4a85bbb41dcb345b586f9494cd073fdf5d78f
Aengus Walton <ventolin@gmail.com>
2014-06-18 20:13:46 +0200

    pep8 cleanup

--- ahye.py
+++ ahye.py
@@ -11,16 +11,16 @@ BASEDIR = os.path.abspath(os.path.dirname(__file__))
 
 COMMANDS = {
     'linux2': {
-        'browser': 'x-www-browser {url}',
-        'screenshot': 'gnome-screenshot -a -f {filename}'
+        'browser': 'x-www-browser {0}',
+        'screenshot': 'gnome-screenshot -a -f {0}'
     },
     'darwin': {
-        'browser': 'open {url}',
-        'screenshot': 'screencapture -s {filename}'
+        'browser': 'open {0}',
+        'screenshot': 'screencapture -s {0}'
     },
     'win32': {
-        'browser': 'start {url}',
-        'screenshot': os.path.join(BASEDIR, 'boxcutter.exe') + ' {filename}'
+        'browser': 'start {0}',
+        'screenshot': os.path.join(BASEDIR, 'boxcutter.exe') + ' {0}'
     }
 }
 
@@ -40,11 +40,13 @@ VERSION = '1.0'
 
 if __name__ == '__main__':
     tmp_file = tempfile.mkstemp(suffix='.png')[1]
-    subprocess.call(COMMANDS[sys.platform]['screenshot'].format(filename=tmp_file).split(' '))
+    command = COMMANDS[sys.platform]['screenshot'].format(tmp_file).split(' ')
+    subprocess.call(command)
     files = {'imagedata': open(tmp_file, 'rb')}
     headers = {'User-Agent': 'ahye-python {0}'.format(VERSION)}
     url = '{0}:{1}{2}'.format(SERVER['host'], SERVER['port'], SERVER['path'])
     resp = requests.post(url, auth=SERVER['auth'], headers=headers,
                          files=files, proxies=PROXIES, verify=False)
-    subprocess.Popen(COMMANDS[sys.platform]['browser'].format(url=resp.text).split(' '))
+    command = COMMANDS[sys.platform]['browser'].format(resp.text).split(' ')
+    subprocess.Popen(command)
     os.remove(tmp_file)
